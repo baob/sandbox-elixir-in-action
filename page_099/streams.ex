24e21ffb0fe9ae66f1ba5cb276651abc677fff64
baob <coder@onesandthrees.com>
2016-09-30 12:39:44 +0100

    lines_length! solution

--- page_099/streams.ex
+++ page_099/streams.ex
@@ -4,4 +4,9 @@ defmodule Streams do
     |> Stream.map(&String.replace(&1, "\n", ""))
     |> Enum.filter(&(String.length(&1) > 80))
   end
+
+  def lines_lengths!(path) do
+    File.stream!(path)
+    |> Enum.map(&String.length(&1))
+  end
 end
