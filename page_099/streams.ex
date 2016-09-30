6923050032691cdc022eb76b7fffc1099b5e0ae6
baob <coder@onesandthrees.com>
2016-09-30 12:30:52 +0100

    streams example from book

---
+++ page_099/streams.ex
@@ -0,0 +1,7 @@
+defmodule Streams do
+  def large_lines!(path) do
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.filter(&(String.length(&1) > 80))
+  end
+end
