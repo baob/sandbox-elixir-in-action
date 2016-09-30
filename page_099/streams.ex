7d7372cde93705367b89c323f7d9d012e5c00fbe
baob <coder@onesandthrees.com>
2016-09-30 15:49:56 +0100

    more streams, more code reuse after seeing given solutions
    See
    https://github.com/sasa1977/elixir-in-action/blob/master/code_samples/ch03/enum_streams_practice.ex

--- page_099/streams.ex
+++ page_099/streams.ex
@@ -1,27 +1,32 @@
 defmodule Streams do
-  def large_lines!(path) do
+  defp file_stream!(path) do
     File.stream!(path)
     |> Stream.map(&String.replace(&1, "\n", ""))
+  end
+
+  def large_lines!(path) do
+    file_stream!(path)
     |> Enum.filter(&(String.length(&1) > 80))
   end
 
   def lines_lengths!(path) do
-    File.stream!(path)
+    file_stream!(path)
     |> Enum.map(&String.length(&1))
   end
 
   def longest_line_length!(path) do
-    File.stream!(path)
+    file_stream!(path)
     |> Enum.reduce(0, &max(String.length(&1), &2))
   end
 
   def longest_line!(path) do
-    File.stream!(path)
-    |> Enum.reduce({0, ''}, &max({String.length(&1), &1}, &2))
+    file_stream!(path)
+    |> Stream.map(&{String.length(&1), &1})
+    |> Enum.reduce({0, ''}, &max(&1, &2))
   end
 
   def words_per_line!(path) do
-    File.stream!(path)
+    file_stream!(path)
     |> Enum.map(&length(String.split(&1)))
   end
 end
